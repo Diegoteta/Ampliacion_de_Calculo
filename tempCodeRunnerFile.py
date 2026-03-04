0146d7dd07b746315d7f3e40d2ed35823c591b3f
Diegoteta <diegosuarezladero@gmail.com>
2026-03-04 12:47:46 +0100

    comrpubep los temporales

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+
+menu = ['1.Añadir un nuevo material','2.Buscar material','3.Actualizar stock','4.Filtrar for tipo','5.Calcular valor total','6.Alerta de reabastecimiento','7.Salir']
+print('-'*30) 
+print('SISTEMA DE GESTIÓN DE LABORATORIO')
+print('-'*30) 
+opcion = input('Seleccione una opción : ')
+for k in menu:
+    print(k)
