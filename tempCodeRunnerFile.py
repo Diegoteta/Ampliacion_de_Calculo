70d1060a703a405f315872906fafb8661aee1d91
Diegoteta <diegosuarezladero@gmail.com>
2026-03-04 14:38:20 +0100

    s
    s

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,8 +1,8 @@
-
-menu = ['1.Añadir un nuevo material','2.Buscar material','3.Actualizar stock','4.Filtrar for tipo','5.Calcular valor total','6.Alerta de reabastecimiento','7.Salir']
-print('-'*30) 
-print('SISTEMA DE GESTIÓN DE LABORATORIO')
-print('-'*30) 
-opcion = input('Seleccione una opción : ')
-for k in menu:
-    print(k)
+lista1= ['luci','anxo','luis','diego']; lista2=[30,2,7,0.1]
+def lista_materiales_tamaños(nombres,tamaños):
+    print('-'*30)
+    print(f"{'MATERIALES':<15} | {'TAMAÑO':<15}") #guardamos15 huecos para los no,bres y 10 para los tamaños y los dividimos con una ralla
+    print('-'*30)
+    for i in range(len(nombres)):
+        print(f"{nombres[i]:<15} | {tamaños[i]:<10}")
+lista_materiales_tamaños(lista1,lista2)
